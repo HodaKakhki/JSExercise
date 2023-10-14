90db2fcd274a1b725b467514f39ee28b07343b2f
hodhodi <amis883@yahoo.com>
2023-10-14 13:48:38 +0330

    validation form

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-let userNameInput=document.querySelector('username')
+let userNameInput = document.querySelector('.username');
+let passwordInput = document.querySelector('.password');
+let modal= document.querySelector('.modal');
+
+
+const validationLogin = () => {
+    let useNameValue=userNameInput.value
+    let passwordValue = passwordInput.value
+    
+    if (useNameValue.length < 4 || passwordValue.length < 8) {
+ modal.style.background = 'rgb(233,28,28'
+        modal.innerHTML='لطفا اطلاعات را درست وارد کنید'
+        modal.style.display = 'inline'
+       
+    }
+    else {
+        modal.style.background = 'green'
+        modal.innerHTML='لاگین با موفقیت انجام شد'
+        modal.style.display='inline'
+    }
+     setTimeout(() => {
+           modal.style.display='none'
+        }, 3000);
+    console.log(useNameValue,passwordValue);
+}
