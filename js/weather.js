d74f7d9305d9a9e5f85fe99713583063ee79a31d
Hoda <35780371+HodaKakhki@users.noreply.github.com>
2024-01-21 10:18:28 +0330

    Update weather.js
    updated to array

--- js/weather.js
+++ js/weather.js
@@ -1,40 +1,40 @@
-let citiesData = {
-  tehran: {
+let citiesData =[
+  {
     city: "Tehran",
     temp: 12,
     weather: "Sunny",
     humidity: 23,
     windspeed: 40,
   },
-  shiraz: {
+ {
     city: "Shiraz",
     temp: 9,
     weather: "windy",
     humidity: 20,
     windspeed: 44,
   },
-  tabriz: {
+{
     city: "Tabriz",
     temp: 2,
     weather: "rainy",
     humidity: 25,
     windspeed: 47,
   },
-  mashhad: {
+  {
     city: "Mashhad",
     temp: 15,
     weather: "cloudy",
     humidity: 43,
     windspeed: 50,
   },
-  esfahan: {
+ {
     city: "Esfahan",
     temp: 18,
     weather: "snowy",
     humidity: 33,
     windspeed: 60,
   },
-};
+];
 
 let $ = document;
 let searchBtn = $.getElementById("search");
@@ -42,7 +42,10 @@ let searchBar = $.querySelector(".search-bar");
 
 searchBtn.addEventListener("click", function () {
   let searchBarValue = searchBar.value;
-  let mainCityDatas = citiesData[searchBarValue];
+  let mainCityDatas = citiesData.find(function(city){
+    return item.city === searchBarValue
+  })
+  
   if (mainCityDatas) {
     $.querySelector(".city").innerHTML = mainCityDatas.city;
     $.querySelector(".temp").innerHTML = mainCityDatas.temp;
